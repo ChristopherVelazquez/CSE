93b5f441c53c33708f08bcd62c41737669169f16
Christopher Velazquez 6038 <hsaa@fresnou.org>
2017-11-28 10:46:44 -0800

    Added to notes

--- Notes.py
+++ Notes.py
@@ -1,1 +1,28 @@
 # This is working
+print ("Hello World")
+
+#Christopher Velazquez
+#(This is Python 2.7)
+
+print (3 + 5)
+print (5 - 3)
+print (3 * 5)
+print (6 / 2)
+print (3**2)
+
+print # this makes a new line. In python 3.6, it looks like:print ()
+print ("See if you can figure this out")
+print(13 % 12)
+
+car_name= "oof"
+car_type= "Lamborgnini Sesto  Elemento"
+car_cylinders = 8
+car_mpg = 9000.1
+
+# Inling printing
+print ("I have a car called %s" % car_name)
+print ("I have a car called %s. It's a %s," % (car_name, car_type))
+
+#Asking for input
+name = raw_input("What is your name ?")#In python 3 it is just called input
+print("Hello %s, "% name)
