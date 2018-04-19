98f4a56ba1467786c5107a8da2cb7259863b7bd2
Christopher Velazquez 6038 <hsaa@fresnou.org>
2018-04-19 10:46:26 -0700

    Working On Characters

--- Christopher Velazquez Characters.py
+++ Christopher Velazquez Characters.py
@@ -1,5 +1,5 @@
 class Character(object):
-    def __init__(self, stat_total, health, description, name, attack, defense,speed, inventory, abilities, experience):
+    def __init__(self, stat_total, health, description, name, attack, defense,speed,magic_defense, inventory, abilities, experience):
 
         self.name = name
         self.stat_total = stat_total
@@ -7,6 +7,7 @@ class Character(object):
         self.description = description
         self.attack_amt = attack
         self.defense = defense
+        self.magic_defense = magic_defense
         self.inventory = inventory
         self.abilities = abilities
         self.speed = speed
@@ -23,10 +24,17 @@ class Character(object):
         if self.health <= 0:
             print("You died...")
 
+
 Rabbid_Wolf = Character(25, 15, "A wolf that looks very hungry.", 'Rabbid Wolf', 2, 3, 5, 'Fangs', None, None)
-Mummy = Character(40, 25, "A mummy brought back to life by some kind of sinister force", 'Mummy', 7, 5, 3, 'Undead Hand',
-                  None, None)
-Skeleton = Character(35, 35, "A pile of bones reanimated",'Skeleton', 5, 5, 2, 'Rusted Sword', None, None)
+Mummy = Character(40, 35, "A mummy brought back to life by some kind of sinister force.", 'Mummy', 7, 5, 2, 0,
+                  'Undead Hand',None, None)
+Skeleton = Character(38, 25, "A pile of bones reanimated by black magic.",'Skeleton', 5, 5, 3, 0, 'Rusted Sword',
+                     None, None)
+Gargoyle = Character(34,25, "Demons summoned from other world.", 'Gargoyle', 4, 3, 2, 0,'Stone_Lance', None, None)
+Servants = Character(0, 35,"Servants of the God of Death.", 'Servants', 8, 6, 4, 3, 'Minor', 'Summon', None)
+
+
+
 
 
 
